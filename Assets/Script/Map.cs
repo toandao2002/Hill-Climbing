fcc6f588ea9b82da6190a78cf9bd7533033e4fb7
Đào Song Toàn <toandaocut2002@gmail.com>
2023-05-28 23:24:32 +0700

    continue

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -14,24 +14,29 @@ public class Map : MonoBehaviour
         int posX = 2;
 
         spr.spline.Clear();
-        spr.spline.InsertPointAt(0, new Vector3(0 * 2, 3, 0));
+        spr.spline.InsertPointAt(0, new Vector3(0  , 3, 0));
         spr.spline.SetTangentMode(0, ShapeTangentMode.Broken);
-        
-        for (int i = 1; i<100; i++)
+        spr.spline.InsertPointAt(1, new Vector3(4, 3, 0));
+        spr.spline.SetTangentMode(1, ShapeTangentMode.Continuous);
+        for (int i = 2; i<100; i++)
         {
             posX +=  Random.Range(7, 14);
             spr.spline.InsertPointAt(i,new Vector3(posX , Random.Range(3,7), 0));
+            var v = spr.spline.GetPosition(i) - spr.spline.GetPosition(i - 1);
+            float deg = Vector3.Angle(v, Vector3.right);
+            if (v.y < 0) deg = -deg;
+           
             if (_ramdom(0.6f))
             {
-                var v = spr.spline.GetPosition(i) - spr.spline.GetPosition(i-1 );
-                float deg = Vector3.Angle( v,Vector3.right );
-                if (v.y < 0) deg = -deg;
+                
                 var _item = Instantiate(itemPref[Random.Range(0, 2)], parItem.transform);
-                _item.transform.position = spr.spline.GetPosition(i - 1) + new Vector3(0,0.6f,0);
+                _item.transform.localPosition = spr.spline.GetPosition(i - 1) + new Vector3(0,0.6f,0);
                 _item.transform.localRotation = Quaternion.Euler(0, 0, deg);
             }
-            
- 
+            //deg = Random.Range(0, 30);
+            spr.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+            spr.spline.SetLeftTangent(i, new Vector3(-1, 0, 0).normalized);
+            spr.spline.SetRightTangent(i, new Vector3(1,0, 0).normalized);
         }
        
         spr.spline.InsertPointAt(100, new Vector3(posX+ 2, 0, 0));
@@ -39,18 +44,12 @@ public class Map : MonoBehaviour
         spr.spline.InsertPointAt(101, new Vector3(0 * 2, 0, 0));
         spr.spline.SetTangentMode(101, ShapeTangentMode.Broken);
         spr.autoUpdateCollider = true ;
-        for (int i = 1; i < 100; i++)
-        {
-           
-            spr.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-            spr.spline.SetLeftTangent(i, new Vector3(-1, 0, 0));
-            spr.spline.SetRightTangent(i, new Vector3(1, 0, 0));
-        }
+     
       
     }
     public bool  _ramdom(float v)
     {
-        return Random.Range(0, 1) >= v;
+        return Random.Range(0f, 1f) >= v;
        
     }
     // Update is called once per frame
